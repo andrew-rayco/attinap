ee48f79a9455d2c042e13b8f13ec94025cc03f3a
Andrew <andy@andycondon.com>
2020-02-17 11:34:40 +1300

    Refactors SleepButton into reusable Button component. Adds awake button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react'
-import SleepButton from './components/SleepButton'
+import Button from './components/Button'
 
 import moment from 'moment'
 
 class App extends Component {
-    state = { sleepTime: 'No time yet' }
+    state = {
+        sleepTime: 'No time yet',
+        awakeTime: 'No time yet'
+    }
 
-    updateTime() {
+    updateTime(time) {
         const now = new Date()
+
         this.setState({
-            sleepTime: moment(now).format('dddd, MMMM Do YYYY, h:mm:ss a')
+            [time]: moment(now).format('dddd, MMMM Do YYYY, h:mm:ss a')
         })
     }
 
     render() {
         return (
             <>
-                <SleepButton
-                    sleepTime={this.state.sleepTime}
-                    updateTime={() => this.updateTime()}
+                <Button
+                    time={this.state.sleepTime}
+                    updateTime={() => this.updateTime('sleepTime')}
+                    title={'Just went to sleep'}
+                />
+                <Button
+                    time={this.state.awakeTime}
+                    updateTime={() => this.updateTime('awakeTime')}
+                    title={'Just woke up'}
                 />
             </>
         )
