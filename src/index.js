9db3bf1ab0abc8c8566e1a9abd24cb6b435a54d6
Andrew <andy@andycondon.com>
2020-02-20 11:20:44 +1300

    Initial firebase config steps

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let database = firebase.database()
+
+function writeUserData(name) {
+    database.ref('dudes/' + 1).set({
+        username: name
+    })
+}
+
+writeUserData('Andy')
+writeUserData('Maria')
+
+ReactDOM.render(<App />, document.getElementById('root'))
