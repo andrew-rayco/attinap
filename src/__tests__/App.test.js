9b7f248c7bda23e3c0c4c92820994d613c1b7512
Andrew <andy@andycondon.com>
2020-03-02 03:00:49 +1300

    More cleanup

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../App'
-
-beforeEach(() => cleanup())
 
 test('App snapshot', () => {
     const { container } = render(<App />)
