da0c6810cbe7073d28ffcde62a7456439cc31893
Andrew <andy@andycondon.com>
2020-03-06 15:35:05 +1300

    Abstracts firebase config credentials

---
+++ src/firebase-config-sample.js
@@ -0,0 +1,9 @@
+export const config = {
+    apiKey: '...',
+    authDomain: '...firebaseapp.com',
+    databaseURL: 'https://....firebaseio.com',
+    projectId: '...',
+    storageBucket: '....appspot.com',
+    messagingSenderId: '...',
+    appId: '...'
+}
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,26 @@
 import * as firebase from 'firebase/app'
 import 'firebase/database'
 
+import { config } from './firebase-config'
+
+const {
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
+} = config
+
 const firebaseConfig = {
-    apiKey: 'AIzaSyCb5zp9ElI0-OEcfQIZNBI_ND43leKwxVc',
-    authDomain: 'attinap2020.firebaseapp.com',
-    databaseURL: 'https://attinap2020.firebaseio.com',
-    projectId: 'attinap2020',
-    storageBucket: 'attinap2020.appspot.com',
-    messagingSenderId: '482919166374',
-    appId: '1:482919166374:web:615a4659ac11fb94445f67'
+    apiKey,
+    authDomain,
+    databaseURL,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId
 }
 
 firebase.initializeApp(firebaseConfig)
