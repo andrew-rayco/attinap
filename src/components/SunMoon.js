90a69566a6ef5b8d30fdf8742bdc1b8cbd985fd8
Andrew <andy@andycondon.com>
2020-03-07 22:48:33 +1300

    Progress

--- src/components/SunMoon.js
+++ src/components/SunMoon.js
@@ -7,13 +7,16 @@ class SunMoon extends Component {
     constructor() {
         super()
 
+        // this.sun = React.createRef()
+
         this.animationIn = this.animationIn.bind(this)
         this.stars = this.stars.bind(this)
     }
 
     componentDidMount() {
-        let pathEnd =
-            'M24.7,38.9c-0.6,0-2.9-0.1-5.5-1.4C18.4,37.1,16,36,14,33.4c-0.9-1.1-3.2-4.1-3.3-8.6c0-0.5,0-2.9,1.2-5.7c0.5-1.2,1.8-3.6,4.4-5.6c1.2-0.9,4.3-3,8.7-2.9c3.6,0,6.2,1.5,7.1,2.1c2.5,1.5,4,3.4,4.5,4.1c0.6,0.9,2.6,3.8,2.6,8c0,5-2.7,8.4-3.5,9.2C35.3,34.5,31.1,39.2,24.7,38.9z'
+        setTimeout(() => {
+            this.animationIn()
+        }, 3000)
     }
 
     morph(path, scale) {
@@ -22,8 +25,11 @@ class SunMoon extends Component {
     }
 
     stars(t, o, d) {
-        let ss = Snap.select('#starGroup')
-        let anim = ss.animate({ transform: t }, 500, mina.easeinout)
+        let { star1, star2, star3, starGroup } = this.refs
+        // let ss = Snap.select('#starGroup')
+        console.log(starGroup, t)
+
+        starGroup.animate({ transform: t }, 500, mina.easeinout)
         star1.animate({ opacity: o }, Number(d[0]), mina.backin)
         star3.animate({ opacity: o }, Number(d[1]), mina.backin)
         star2.animate({ opacity: o }, Number(d[2]), mina.backin)
@@ -31,9 +37,27 @@ class SunMoon extends Component {
 
     /******* Initial animation (Moon) ********/
     animationIn() {
+        // undefined
+        // let star1 = Snap.select('#star1')
+        // let star2 = Snap.select('#star2')
+        // let star3 = Snap.select('#star3')
+        let sunRay1 = Snap.select('#sunRay2')
+        let sunRay2 = Snap.select('#sunRay4')
+        let sunRay3 = Snap.select('#sunRay8')
+        let sunRay4 = Snap.select('#sunRay6')
+        let sunRay5 = Snap.select('#sunRay1')
+        let sunRay6 = Snap.select('#sunRay3')
+        let sunRay7 = Snap.select('#sunRay7')
+        let sunRay8 = Snap.select('#sunRay5')
         // SVG path animation
-        // Not sure what to do with these. Refs?
-        ReactDOM.findDOMNode('#sun').css({ fill: '#2B77A0' })
+        let { sun, star1, star2, star3 } = this.refs
+
+        sun.style.fill = '#2B77A0'
+        console.log(star1)
+        star1.style.fill = '#FFC928'
+        star2.style.fill = '#FFCF42'
+        star3.style.fill = '#FFCC35'
+        console.log(star1)
         // document.getElementById('#star1').css({ fill: '#FFC928' })
         // document.getElementById('#star2').css({ fill: '#FFCF42' })
         // document.getElementById('#star3').css({ fill: '#FFCC35' })
@@ -75,6 +99,8 @@ class SunMoon extends Component {
         let sunRay6 = Snap.select('#sunRay3')
         let sunRay7 = Snap.select('#sunRay7')
         let sunRay8 = Snap.select('#sunRay5')
+        let pathEnd =
+            'M24.7,38.9c-0.6,0-2.9-0.1-5.5-1.4C18.4,37.1,16,36,14,33.4c-0.9-1.1-3.2-4.1-3.3-8.6c0-0.5,0-2.9,1.2-5.7c0.5-1.2,1.8-3.6,4.4-5.6c1.2-0.9,4.3-3,8.7-2.9c3.6,0,6.2,1.5,7.1,2.1c2.5,1.5,4,3.4,4.5,4.1c0.6,0.9,2.6,3.8,2.6,8c0,5-2.7,8.4-3.5,9.2C35.3,34.5,31.1,39.2,24.7,38.9z'
 
         // SVG path animation
         // $('#sun').css({ fill: '#FFEC50' })
@@ -86,14 +112,14 @@ class SunMoon extends Component {
 
         // Sunrays animation
         setTimeout(function() {
-            sunRay1.animate({ opacity: 1 }, 55, mina.easing)
-            sunRay2.animate({ opacity: 1 }, 110, mina.easing)
-            sunRay3.animate({ opacity: 1 }, 220, mina.easing)
-            sunRay4.animate({ opacity: 1 }, 275, mina.easing)
-            sunRay5.animate({ opacity: 1 }, 330, mina.easing)
-            sunRay6.animate({ opacity: 1 }, 385, mina.easing)
-            sunRay7.animate({ opacity: 1 }, 440, mina.easing)
-            sunRay8.animate({ opacity: 1 }, 495, mina.easing)
+            this.sunRay1.animate({ opacity: 1 }, 55, mina.easing)
+            this.sunRay2.animate({ opacity: 1 }, 110, mina.easing)
+            this.sunRay3.animate({ opacity: 1 }, 220, mina.easing)
+            this.sunRay4.animate({ opacity: 1 }, 275, mina.easing)
+            this.sunRay5.animate({ opacity: 1 }, 330, mina.easing)
+            this.sunRay6.animate({ opacity: 1 }, 385, mina.easing)
+            this.sunRay7.animate({ opacity: 1 }, 440, mina.easing)
+            this.sunRay8.animate({ opacity: 1 }, 495, mina.easing)
         }, 300)
     }
     /*****************************************/
@@ -115,17 +141,17 @@ class SunMoon extends Component {
                 setTimeout(() => {
                     this.animationBack()
                 }, 2000)
-            }, 500)
+            }, 2000)
         }, 3500)
     }
 
     render() {
-        // this.animController()
         return (
             <div className="svg-wrapper">
                 <div className="sunmoon-container">
                     <svg viewBox="0 0 50 50">
                         <path
+                            ref={'sun'}
                             id="sun"
                             d="M24.7,38.9c-0.6,0-2.9-0.1-5.5-1.4C18.4,37.1,16,36,14,33.4c-0.9-1.1-3.2-4.1-3.3-8.6
             c0-0.5,0-2.9,1.2-5.7c0.5-1.2,1.8-3.6,4.4-5.6c1.2-0.9,4.3-3,8.7-2.9c3.6,0,6.2,1.5,7.1,2.1c2.5,1.5,4,3.4,4.5,4.1
@@ -171,18 +197,25 @@ class SunMoon extends Component {
                                 d="M45.7,26.7h-4c-0.7,0-1.3-0.6-1.3-1.3s0.6-1.3,1.3-1.3h4c0.7,0,1.3,0.6,1.3,1.3S46.4,26.7,45.7,26.7z"
                             />
                         </g>
-                        <g id="starGroup" transform="translate(5 0)">
+                        <g
+                            ref={'starGroup'}
+                            id="starGroup"
+                            transform="translate(5 0)"
+                        >
                             <polygon
+                                ref={'star1'}
                                 id="star1"
                                 points="29.4,25.5 30,27 30.5,25.4 32.4,25.4 30.8,24.5 31.4,22.9 29.9,23.9 28.4,23 29,24.5 
     27.5,25.5 "
                             />
                             <polygon
+                                ref={'star2'}
                                 id="star2"
                                 points="30.2,29.9 28.9,27.9 28.6,30.2 25.9,30.7 28.4,31.6 28.1,33.9 29.9,32.2 32.4,33.2 
     31,31.2 32.9,29.4 "
                             />
                             <polygon
+                                ref={'star3'}
                                 id="star3"
                                 points="23.5,29.3 24.6,28.5 25.8,29.3 25.4,28.1 26.5,27.3 25.1,27.3 24.6,26.2 24.2,27.3 
     22.8,27.3 23.9,28.1 "
