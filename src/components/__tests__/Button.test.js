5fbefce1bb037c8ed97cc4ac17ac0f646ef50cf4
Andrew <andy@andycondon.com>
2020-04-13 00:23:21 +1200

    Resolves testing errors w/ missing required props

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -3,48 +3,68 @@ import ReactDOM from 'react-dom'
 import Button from '../Button'
 import { render } from '@testing-library/react'
 
+const requiredProps = {
+  updateTime: jest.fn(),
+  deleteLast: jest.fn(),
+  addTime: jest.fn(),
+  time: 'This is the time',
+  isClockOpen: false,
+  className: 'cheese'
+}
+
 test('Renders correct button text with closed clock', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<Button isClockOpen={false} />, container)
-    expect(container.textContent).toMatch('Undo last')
-    expect(container.textContent).toMatch('Add time')
+  const container = document.createElement('div')
+  ReactDOM.render(
+    <Button {...requiredProps} isClockOpen={false} className={'cheese'} />,
+    container
+  )
+  expect(container.textContent).toMatch('Undo last')
+  expect(container.textContent).toMatch('Add time')
 })
 
 test('Renders correct button text with open clock', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<Button isClockOpen={true} />, container)
-    expect(container.textContent).toMatch('Undo last')
-    expect(container.textContent).toMatch('Close clock')
-    expect(container.firstChild.className).toMatch('btn')
+  const container = document.createElement('div')
+  ReactDOM.render(<Button {...requiredProps} isClockOpen={true} />, container)
+  expect(container.textContent).toMatch('Undo last')
+  expect(container.textContent).toMatch('Close clock')
+  expect(container.firstChild.className).toMatch('btn')
 })
 
 test('Renders main div with given className', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<Button className={'cheese'} />, container)
-    expect(container.firstChild.className).toMatch('btn')
-    expect(container.firstChild.className).toMatch('cheese')
+  const container = document.createElement('div')
+  ReactDOM.render(<Button {...requiredProps} className={'cheese'} />, container)
+  expect(container.firstChild.className).toMatch('btn')
+  expect(container.firstChild.className).toMatch('cheese')
 })
 
 test('Renders given title', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<Button title={'Wormwood Industrial'} />, container)
-    expect(container.textContent).toMatch('Wormwood Industrial')
+  const container = document.createElement('div')
+  ReactDOM.render(
+    <Button {...requiredProps} title={'Wormwood Industrial'} />,
+    container
+  )
+  expect(container.textContent).toMatch('Wormwood Industrial')
 })
 
 test('Renders `no time yet today` if no time given', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<Button time={'No time yet today'} />, container)
+  const container = document.createElement('div')
+  ReactDOM.render(
+    <Button {...requiredProps} time={'No time yet today'} />,
+    container
+  )
 
-    expect(container.textContent).toMatch('No time yet today')
+  expect(container.textContent).toMatch('No time yet today')
 })
 
 test('Renders time string if time given', () => {
-    const { getByTestId } = render(<Button time={'this is the time'} />)
-    const timeNode = getByTestId('time')
-    expect(timeNode.textContent).toEqual('this is the time')
+  const { getByTestId } = render(
+    <Button {...requiredProps} time={'this is the time'} />
+  )
+  const timeNode = getByTestId('time')
+  expect(timeNode.textContent).toEqual('this is the time')
 })
 
 test('Button snapshot', () => {
-    const { container } = render(<Button />)
-    expect(container).toMatchSnapshot()
+  const { container } = render(<Button {...requiredProps} />)
+  expect(container).toMatchSnapshot()
 })
--- src/components/__tests__/TickTock.test.js
+++ src/components/__tests__/TickTock.test.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom'
 import { render } from '@testing-library/react'
 import TickTock from '../TickTock'
 
+const requiredProps = {
+  handleChange: jest.fn(),
+  handleClockSubmit: jest.fn()
+}
+
 test('Renders button with `submit time` text', () => {
-    const container = document.createElement('div')
-    ReactDOM.render(<TickTock />, container)
-    expect(container.textContent).toMatch('Submit time')
+  const container = document.createElement('div')
+  ReactDOM.render(<TickTock {...requiredProps} />, container)
+  expect(container.textContent).toMatch('Submit time')
 })
 
 // Only snapshotting button. No need to test Timepicker module
 test('TickTock button snapshot', () => {
-    const { getByText } = render(<TickTock />)
-    const submitTimeButton = getByText('Submit time')
+  const { getByText } = render(<TickTock {...requiredProps} />)
+  const submitTimeButton = getByText('Submit time')
 
-    expect(submitTimeButton).toMatchSnapshot()
+  expect(submitTimeButton).toMatchSnapshot()
 })
